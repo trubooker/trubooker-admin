45444e03459700a8a4c00fbc8b4d2216ead28335
Uchenna Ofor <ucheofor2015@gmail.com>
2024-12-14 22:38:33 +0100

    notification done

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,8 +1,11 @@
-import { IoTrashOutline } from "react-icons/io5";
-import { Avatar } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { Card, CardHeader, CardContent } from "@/components/ui/card";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
 import Image from "next/image";
 import { useState } from "react";
 import Spinner from "./Spinner";
@@ -12,7 +15,22 @@ import {
   useDeleteOneNotificationMutation,
   useFetchNotificationsQuery,
   useMarkAllAsReadMutation,
+  useMarkOneAsReadMutation,
 } from "@/redux/services/Slices/notificationApiSlice";
+import { MdDeleteForever } from "react-icons/md";
+import { IoMailUnreadOutline } from "react-icons/io5";
+import { GoRead } from "react-icons/go";
+import { BsCheckAll } from "react-icons/bs";
+import { Badge } from "@/components/ui/badge";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Notification } from "@/constants";
+import SwipeableNotification from "./SwipeableContent";
+import { Modal } from "./DualModal";
 
 const Notifications = () => {
   const [viewType, setViewType] = useState<"unread" | "read">("unread");
@@ -26,6 +44,7 @@ const Notifications = () => {
     useDeleteAllNotificationsMutation();
   const [deleteOne, { isLoading: deleteOneLoading }] =
     useDeleteOneNotificationMutation();
+  const [markOne, { isLoading: markOneLoading }] = useMarkOneAsReadMutation();
 
   const handleMarkAllAsRead = async () => {
     try {
@@ -51,93 +70,163 @@ const Notifications = () => {
     }
   };
 
+  const handleMarkOne = async (id: string) => {
+    try {
+      await markOne(id);
+    } catch (error) {
+      console.error("Failed to delete notification:", error);
+    }
+  };
+
   return (
     <div>
-      <Card className="w-full overflow-y-auto h-[500px]">
-        <CardHeader className="sticky pt-5 pb-3 bg-white text-left text-lg font-bold flex justify-between items-center">
-          <span>Notifications</span>
-          <div className="flex space-x-2">
-            <Button
-              variant="outline"
-              size="sm"
-              disabled={isLoading || isFetching || deleteAllLoading}
-              onClick={() => setViewType("unread")}
-            >
-              Unread
-            </Button>
-            <Button
-              variant="outline"
-              size="sm"
-              disabled={isLoading || isFetching || deleteAllLoading}
-              onClick={() => setViewType("read")}
-            >
-              Read
-            </Button>
-            <Button
-              variant="destructive"
-              size="sm"
-              disabled={deleteAllLoading}
-              onClick={handleDeleteAll}
-            >
-              Delete All
-            </Button>
-          </div>
-        </CardHeader>
-        <CardContent>
-          {isFetching || isLoading ? (
-            <div className="h-[390px] w-full flex justify-center items-center">
-              <Spinner />
-            </div>
-          ) : data?.length > 0 ? (
-            <>
-              {data.map((notification: any) => (
-                <div key={notification.id}>
-                  <Separator />
-                  <div className="my-4 flex justify-between items-start">
-                    <div className="flex items-start space-x-4">
-                      <Image
-                        src={Logo}
-                        width={40}
-                        alt="Logo"
-                        className="flex"
-                      />
-                      <div>
-                        <p className="text-gray-800 font-medium text-sm">
-                          {notification.message}
-                        </p>
-                        <p className="text-xs text-gray-500">
-                          {notification.date}
-                        </p>
-                      </div>
-                    </div>
-                    <Button
-                      variant="ghost"
-                      size="icon"
-                      disabled={deleteOneLoading}
-                      onClick={() => handleDeleteOne(notification.id)}
+      <div className="">
+        <Card className="w-full overflow-y-auto overflow-x-hidden max-h-[500px]">
+          <CardHeader className="sticky pt-4 pb-2 px-5 bg-white shadow-lg top-0 z-50 border-b text-left text-lg font-bold flex flex-row justify-between items-start">
+            <span>
+              Notifications
+              <div>
+                {viewType === "read" ? (
+                  <small>
+                    <Badge
+                      variant="outline"
+                      className="text-[--primary] border-[--primary] mb-1"
                     >
-                      <IoTrashOutline size={20} />
-                    </Button>
-                  </div>
-                </div>
-              ))}
+                      Seen
+                    </Badge>
+                  </small>
+                ) : (
+                  <Badge
+                    variant="outline"
+                    className="text-[--primary] border-[--primary] mb-1"
+                  >
+                    Unread
+                  </Badge>
+                )}
+              </div>
+            </span>
+            <span>
+              {viewType === "read" ? (
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <IoMailUnreadOutline
+                        onClick={() => setViewType("unread")}
+                        className="text-[--primary] cursor-pointer w-6 h-6 mx-auto"
+                      />
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>View Unread Messages</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              ) : (
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <GoRead
+                        onClick={() => setViewType("read")}
+                        className="text-[--primary] cursor-pointer w-6 h-6 mx-auto"
+                      />
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>View Read Messages</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              )}
+            </span>
+          </CardHeader>
+
+          <CardContent className="py-3 px-2">
+            <>
+              {Notification?.length > 0 ? (
+                <>
+                  {Notification?.map((notification: any) => (
+                    <SwipeableNotification
+                      key={notification.id}
+                      id={notification.id}
+                      onMarkAsRead={handleMarkOne}
+                      onDelete={handleDeleteOne}
+                      content={
+                        <div className="h-[100px]">
+                          <div className="flex w-full items-start h-full space-x-4">
+                            <Image
+                              src={Logo}
+                              width="40"
+                              alt="Logo"
+                              className=" flex "
+                            />
+                            <div className="h-full">
+                              <p className="text-gray-800 font-medium text-xs flex">
+                                {notification.message}
+                              </p>
+                              <small className="bottom-0 left-0 mt-auto h-full text-gray-500">
+                                {notification.date}
+                              </small>
+                            </div>
+                          </div>
+                        </div>
+                      }
+                    />
+                  ))}
+                </>
+              ) : (
+                <>
+                  {isFetching ? (
+                    <div className="h-[330px] w-full">
+                      <Spinner />
+                    </div>
+                  ) : (
+                    <div className="flex items-center w-full h-[330px] flex-col justify-center">
+                      <Image
+                        src={"/nodata.svg"}
+                        alt=""
+                        width={160}
+                        height={160}
+                        className="object-cover me-5"
+                      />
+                      <h1 className="mt-8 text-lg text-center font-semibold">
+                        You are all caught up
+                      </h1>
+                    </div>
+                  )}
+                </>
+              )}
             </>
-          ) : (
-            <div className="flex flex-col items-center justify-center h-[390px]">
-              <Image
-                src="/nodata.svg"
-                alt="No data"
-                width={200}
-                height={200}
-                className="object-cover"
-              />
-              <h1 className="mt-8 text-lg font-semibold">
-                You are all caught up
-              </h1>
+          </CardContent>
+          <CardFooter className="sticky z-50 bottom-0 bg-white shadow-lg border-t px-5 py-3">
+            <div className="flex gap-x-2 justify-end ms-auto">
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <BsCheckAll
+                      onClick={handleMarkAllAsRead}
+                      className="text-[--primary] rounded-full cursor-pointer w-10 h-8 mx-auto"
+                    />
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Mark all Notifications as Read</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <MdDeleteForever
+                      onClick={handleDeleteAll}
+                      className="text-red-500 rounded-full cursor-pointer w-10 h-8 mx-auto"
+                    />
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Delete all Notifications</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
             </div>
-          )}
-        </CardContent>
-      </Card>
+          </CardFooter>
+        </Card>
+      </div>
     </div>
   );
 };
