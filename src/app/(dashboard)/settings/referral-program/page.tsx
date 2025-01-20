ff97e437fbaa2db56df93f0e12135721d6650489
Uchenna Ofor <ucheofor2015@gmail.com>
2025-01-20 10:24:39 +0100

    chore: updates

--- src/app/(dashboard)/settings/referral-program/page.tsx
+++ src/app/(dashboard)/settings/referral-program/page.tsx
@@ -34,7 +34,7 @@ const Referral_program = () => {
     isFetching,
   } = useGetReferralProgramsQuery(null);
 
-  const totalPages = info?.data?.active_trips?.last_page;
+  const totalPages = info?.data?.referral_performance?.meta?.last_page;
   const revenue = info?.data;
   const ReferralProgramListData = info?.data?.referral_performance?.data;
   const onPageChange = (pageNumber: number) => {
@@ -50,7 +50,6 @@ const Referral_program = () => {
           <h2 className="text-2xl font-bold">Referral Program</h2>
         </div>
         <div className="grid grid-cols-1 xl:grid-cols-3 w-full gap-4">
-
           <div className="flex flex-col gap-y-10 lg:col-span-2 w-full ">
             <div className="w-full">
               <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
@@ -159,103 +158,114 @@ const Referral_program = () => {
                 </div>
               </div>
               {ReferralProgramListData?.length > 0 ? (
-                <ScrollArea className="w-full">
-                  {loading ? (
-                    <>
-                      <Table className="min-w-[700px] py-2">
-                        <TableHeader>
-                          <TableRow className="text-xs lg:text-sm text-center">
-                            <TableHead className="font-bold w-1/4 text-left">
-                              Passenger name
-                            </TableHead>
-                            <TableHead className="font-bold w-1/4 text-center">
-                              Earnings
-                            </TableHead>
-                            <TableHead className="font-bold w-1/4 text-center">
-                              Status
-                            </TableHead>
-                            <TableHead className="w-1/4 text-center">
-                              Actions
-                            </TableHead>
-                          </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                          {[1, 2, 3, 4, 5, 6, 7].map((i) => (
-                            <TableRow key={i}>
-                              {[1, 2, 3, 4].map((i) => (
-                                <TableCell key={i}>
-                                  <div>
-                                    <div className="w-full rounded-md">
-                                      <div>
-                                        <Skeleton className="h-4 w-1/7 bg-gray-400" />
+                <>
+                  <ScrollArea className="w-full">
+                    {loading ? (
+                      <>
+                        <Table className="min-w-[700px] py-2">
+                          <TableHeader>
+                            <TableRow className="text-xs lg:text-sm text-center">
+                              <TableHead className="font-bold w-1/4 text-left">
+                                Passenger name
+                              </TableHead>
+                              <TableHead className="font-bold w-1/4 text-center">
+                                Earnings
+                              </TableHead>
+                              <TableHead className="font-bold w-1/4 text-center">
+                                Status
+                              </TableHead>
+                              <TableHead className="w-1/4 text-center">
+                                Actions
+                              </TableHead>
+                            </TableRow>
+                          </TableHeader>
+                          <TableBody>
+                            {[1, 2, 3, 4, 5, 6, 7].map((i) => (
+                              <TableRow key={i}>
+                                {[1, 2, 3, 4].map((i) => (
+                                  <TableCell key={i}>
+                                    <div>
+                                      <div className="w-full rounded-md">
+                                        <div>
+                                          <Skeleton className="h-4 w-1/7 bg-gray-400" />
+                                        </div>
                                       </div>
                                     </div>
+                                  </TableCell>
+                                ))}
+                              </TableRow>
+                            ))}
+                          </TableBody>
+                        </Table>
+                      </>
+                    ) : (
+                      <ScrollArea>
+                        <Table className="min-w-[700px] py-2">
+                          <TableHeader>
+                            <TableRow className="text-xs lg:text-sm text-center">
+                              <TableHead className="font-bold w-1/5 text-left">
+                                Passenger name
+                              </TableHead>
+                              <TableHead className="font-bold w-1/5 text-center">
+                                Referrals made
+                              </TableHead>
+                              <TableHead className="font-bold w-1/5 text-center">
+                                Used Coupons
+                              </TableHead>
+                              <TableHead className="w-1/5 font-bold text-center">
+                                Unused Coupons
+                              </TableHead>
+                              <TableHead className="w-1/5 font-bold text-center">
+                                Expired Coupons
+                              </TableHead>
+                            </TableRow>
+                          </TableHeader>
+                          <TableBody>
+                            {ReferralProgramListData?.map((data: any) => (
+                              <TableRow
+                                key={data.id}
+                                className="text-xs text-center lg:text-sm"
+                              >
+                                <TableCell className=" py-5 w-1/5 text-left">
+                                  <div className="w-full flex gap-x-3 items-center">
+                                    <span className="w-full flex flex-col gap-x-2 gap-y-1 text-gray-500">
+                                      <span className="font-semibold">
+                                        {data.name}
+                                      </span>
+                                    </span>
                                   </div>
                                 </TableCell>
-                              ))}
-                            </TableRow>
-                          ))}
-                        </TableBody>
-                      </Table>
-                    </>
-                  ) : (
-                    <ScrollArea>
-                      <Table className="min-w-[700px] py-2">
-                        <TableHeader>
-                          <TableRow className="text-xs lg:text-sm text-center">
-                            <TableHead className="font-bold w-1/4 text-left">
-                              Passenger name
-                            </TableHead>
-                            <TableHead className="font-bold w-1/4 text-center">
-                              Referrals made
-                            </TableHead>
-                            <TableHead className="font-bold w-1/4 text-center">
-                              Target Referrals
-                            </TableHead>
-                            <TableHead className="w-1/4 font-bold text-center">
-                              Coupon issued
-                            </TableHead>
-                          </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                          {ReferralProgramListData?.map((data: any) => (
-                            <TableRow
-                              key={data.id}
-                              className="text-xs text-center lg:text-sm"
-                            >
-                              <TableCell className=" py-5 w-1/4 text-left">
-                                <div className="w-full flex gap-x-3 items-center">
-                                  <Avatar className="w-8 h-8">
-                                    <AvatarImage src={data?.profile_picture} />
-                                    <AvatarFallback>
-                                      <IoPersonOutline />
-                                    </AvatarFallback>
-                                  </Avatar>
-                                  <span className="w-full flex flex-col gap-x-2 gap-y-1 text-gray-500">
-                                    <span className="font-semibold">
-                                      {data.name}
-                                    </span>
-                                  </span>
-                                </div>
-                              </TableCell>
-                              <TableCell className=" py-5 w-1/4">
-                                {data.referrals}
-                              </TableCell>
-                              <TableCell className="py-5 w-1/4">
-                                {data?.target_referrals}
-                              </TableCell>
-                              <TableCell className=" py-5 text-center w-1/4">
-                                {data?.coupon}
-                              </TableCell>
-                            </TableRow>
-                          ))}
-                        </TableBody>
-                      </Table>
-                      <ScrollBar orientation="horizontal" />
-                    </ScrollArea>
+                                <TableCell className=" py-5 w-1/5">
+                                  {data.referrals_made}
+                                </TableCell>
+                                <TableCell className="py-5 w-1/5">
+                                  {data?.used_coupons}
+                                </TableCell>
+                                <TableCell className=" py-5 text-center w-1/5">
+                                  {data?.unused_coupons}
+                                </TableCell>
+                                <TableCell className=" py-5 text-center w-1/5">
+                                  {data?.expired_coupons}
+                                </TableCell>
+                              </TableRow>
+                            ))}
+                          </TableBody>
+                        </Table>
+                        <ScrollBar orientation="horizontal" />
+                      </ScrollArea>
+                    )}
+                    <ScrollBar orientation="horizontal" />
+                  </ScrollArea>
+                  {totalPages > 1 && (
+                    <div className="pt-10">
+                      <Pagination
+                        currentPage={page}
+                        totalPages={totalPages}
+                        onPageChange={onPageChange}
+                      />
+                    </div>
                   )}
-                  <ScrollBar orientation="horizontal" />
-                </ScrollArea>
+                </>
               ) : (
                 <div className="flex flex-col items-center justify-center h-[330px]">
                   <Image
@@ -285,7 +295,6 @@ const Referral_program = () => {
             <ReferralProgram />
             <PriceControl />
           </div>
-
         </div>
       </div>
     </div>
