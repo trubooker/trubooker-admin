7c6b49bfd6e9ce4330d589c2a3a5a36d579a2510
Papilo <shazaniyu@gmail.com>
2026-07-21 15:13:16 +0100

    feature/admin

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,6 +6,14 @@ export interface LinkItem {
   sublinks?: any[];
 }
 
+export interface NiyuItem {
+  id: number;
+  title: string;
+  link?: string;
+  icon: any;
+  sublinks?: any[];
+}
+
 export interface ModalProps {
   data: any;
   onModalClose?: () => void;
